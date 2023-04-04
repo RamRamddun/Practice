9bd62c18596006e55a6713eb3472ed69952e55d9
RaM <0wines.pumpkin@icloud.com>
2023-04-04 12:08:57 +0900

    portfolio practice

---
+++ portfolio/main.js
@@ -0,0 +1,96 @@
+'use strict';
+
+// make navbar transparent top and color when scrolling;
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+document.addEventListener('scroll',() => {
+    if(window.scrollY > navbarHeight){
+        navbar.classList.add('navbar--dark');
+    }else{
+        navbar.classList.remove('navbar--dark');
+    }
+});
+
+
+// handle scrolling when tap navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if(link == null){
+        return;
+    }
+    navbarMenu.classList.remove('open');
+    scrollIntoViews(link);
+});
+
+// navbar toggle button media querry
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+    navbarMenu.classList.toggle('open');
+});
+
+// handle click "contact me" button
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', () => {
+    scrollIntoViews('#contact');
+});
+
+// make home slowly fade when scroll
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () =>{
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+// show arrow up when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () =>{
+    if(window.scrollY > homeHeight / 2){
+        arrowUp.classList.add('visible');
+    }else{
+        arrowUp.classList.remove('visible');
+    }
+});
+
+// handle click arrow up
+arrowUp.addEventListener('click',() =>{
+    scrollIntoViews('#home');
+})
+
+//projects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if(filter == null){
+        return;
+    }
+
+    // remove selection and select the newone
+    const active = document.querySelector('.category__btn.selected');
+    active.classList.remove('selected');
+    const target = e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
+    target.classList.add('selected');
+
+
+    projectContainer.classList.add('anime-out');
+    setTimeout(() => {
+        projects.forEach((project) => {
+            if(filter === '*' || filter === project.dataset.type) {
+                project.classList.remove('invisible');
+            }else{
+                project.classList.add('invisible');
+            }
+        });
+        projectContainer.classList.remove('anime-out');
+    },300);
+});
+
+function scrollIntoViews(selector){
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: 'smooth'}); // behavior is not working on safari
+}
+
