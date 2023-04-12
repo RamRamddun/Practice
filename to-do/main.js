a8b81a0dfc851fe8db695e6abd4dab308c0f92c4
RaM <0wines.pumpkin@icloud.com>
2023-04-12 15:37:50 +0900

    setUp js variables

--- to-do/main.js
+++ to-do/main.js
@@ -1,4 +1,10 @@
 'use strict';
 
 const items = document.querySelector('.lists');
-const input = document.querySelector('.input_textbox')
+const input = document.querySelector('.input_textbox');
+const add = document.querySelector('.input__btn');
+const form = document.querySelector('.input__form');
+const count = document.querySelector('.list__count');
+
+
+
