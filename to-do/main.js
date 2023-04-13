86112984346e979428be66b5816f6c7d9af7fbe1
RaM <0wines.pumpkin@icloud.com>
2023-04-13 12:37:40 +0900

    add form submit fucntion & onAdd()

--- to-do/main.js
+++ to-do/main.js
@@ -1,10 +1,20 @@
 'use strict';
 
 const items = document.querySelector('.lists');
-const input = document.querySelector('.input_textbox');
+const input = document.querySelector('.input__textbox');
 const add = document.querySelector('.input__btn');
 const form = document.querySelector('.input__form');
 const count = document.querySelector('.list__count');
 
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  onAdd();
+});
 
-
+function onAdd() {
+  const text = input.value;
+  if (text == '') {
+    input.focus();
+    return;
+  }
+}
