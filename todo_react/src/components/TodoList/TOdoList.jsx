51d0aa002a45420f6efd186897c7abe5ed03628a
RaM <0wines.pumpkin@icloud.com>
2023-05-10 12:01:06 +0900

    test list

--- todo_react/src/components/TodoList/TOdoList.jsx
+++ todo_react/src/components/TodoList/TOdoList.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function TodoList() {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [todos, setTodo] = useState([
+    { id: '123', text: 'test', status: 'active' },
+    { id: '124', text: 'test2', status: 'active' },
+  ]);
+  return (
+    <section>
+      <ul>
+        {todos.map((list) => (
+          <li key={list.id}>{list.text}</li>
+        ))}
+      </ul>
+    </section>
+  );
 }
-
