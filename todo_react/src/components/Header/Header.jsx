835051d8c759d69d012571fb4657f848c1fcb58c
Ram <0wines.pumpkin@icloud.com>
2023-06-01 17:11:58 +0900

    Style : add css in header component

--- todo_react/src/components/Header/Header.jsx
+++ todo_react/src/components/Header/Header.jsx
@@ -3,12 +3,19 @@ import styles from './Header.module.css';
 
 export default function Header({ list, current, onFilterChange }) {
   return (
-    <header>
-      <h1>do it</h1>
-      <ul>
+    <header className={styles.header}>
+      <h1 className={styles.title}>do it</h1>
+      <ul className={styles.filterList}>
         {list.map((value, index) => (
           <li key={index}>
-            <button onClick={() => onFilterChange(value)}>{value}</button>
+            <button
+              className={`${styles.filter} ${
+                current === value && styles.selected
+              }`}
+              onClick={() => onFilterChange(value)}
+            >
+              {value}
+            </button>
           </li>
         ))}
       </ul>
